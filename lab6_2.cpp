2cd6ad5a544b2e7645dd22e6eb393f84c25be4f6
Nithit Suepin <nithit_su@cmu.ac.th>
2022-12-09 23:28:58 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,29 +1,48 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
-int main()
-{
+double deg2rad(double deg){
+   return deg*(M_PI/180);
 
-    int N;
-    int odd = 0;
-    int even = 0;
-    while ( N != 0)
-    {
-        cout << "Enter an integer: ";
-        cin >> N;
-            if(N%2 == 0){
-                even = even+1; 
-            if(N == 0){
-                even = even-1;
-            } 
-            }else{
-                odd = odd+1;
-            }
+}
+double rad2deg(double rad){
+   return rad*(180/M_PI);
+
+}
+double findXComponent(double l1,double l2 ,double a1,double a2){
+  return (l1*cos(a1))+(l2*cos(a2));
+}
+double findYComponent(double l1,double l2 ,double a1,double a2){
+  return (l1*sin(a1))+(l2*sin(a2));
+}
+double pythagoras(double xcomp, double ycomp){
+  return sqrt(pow(xcomp,2)+pow(ycomp,2));
+}
+void showResult(double o,double p){ 
+cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << endl;
+cout << "Length of the resultant vector = " << o << endl;
+cout << "Direction of the resultant vector (deg) = " << p << endl;
+cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+}
+int main(){
+    double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
+    cout << "Enter length of the first vector: ";
+    cin >> l1;
+    cout << "Enter direction of the first vector (deg): ";
+    cin >> a1;
+    cout << "Enter length of the second vector: ";
+    cin >> l2;
+    cout << "Enter direction of the second vector (deg): ";
+    cin >> a2;
     
-    }
+    a1 = deg2rad(a1);
+    a2 = deg2rad(a2);
+    xcomp = findXComponent(l1,l2,a1,a2);
+    ycomp = findYComponent(l1,l2,a1,a2);
+    result_vec_length = pythagoras(xcomp,ycomp);
+    result_vec_direction = rad2deg(atan2(ycomp,xcomp)); 
 
-    cout << "#Even numbers = " << even << "\n";
-    cout << "#Odd numbers = " << odd;
-
-    return 0;
+    showResult(result_vec_length,result_vec_direction);
 }
